7786815bc3b4cad719f1874383bf08ec19f51658
tkillman <dkkim1004@hanmail.net>
2017-03-14 21:25:42 +0900

    afd

--- src/main/java/spring/chap15/Dao.java
+++ src/main/java/spring/chap15/Dao.java
@@ -3,41 +3,46 @@ package spring.chap15;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class Dao {
+public class Dao extends SqlSessionDaoSupport{
 
-	@Autowired
+	/*@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
 
 	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
 		this.sqlSessionTemplate = sqlSessionTemplate;
-	}
+	}*/
 	
 	//list 꺼내오기
 	public List<Dto> getList(){
 		
-		List<Dto> list =sqlSessionTemplate.selectList("mapperNamespace.getList");
+		//List<Dto> list =sqlSessionTemplate.selectList("mapperNamespace.getList");
+		List<Dto> list =getSqlSession().selectList("mapperNamespace.getList");
 		return list;
 		
 	}
 	
 	//insert 해보기
 	public int insertMember(Dto dto){
-		int resultNum =sqlSessionTemplate.insert("mapperNamespace.insertMember", dto);
+		//int resultNum =sqlSessionTemplate.insert("mapperNamespace.insertMember", dto);
+		int resultNum =getSqlSession().insert("mapperNamespace.insertMember", dto);
 		return resultNum;
 	}
 	
 	
 	//update 해보기
 	public int updateMember(Dto dto){
-		int resultNum =sqlSessionTemplate.update("mapperNamespace.updateMember", dto);
+		//int resultNum =sqlSessionTemplate.update("mapperNamespace.updateMember", dto);
+		int resultNum =getSqlSession().update("mapperNamespace.updateMember", dto);
 		return resultNum;
 	}
 	
 	//delete 해보기
 	public int deleteMember(String memberId){
-		int resultNum = sqlSessionTemplate.delete("mapperNamespace.deleteMember",memberId);
+		//int resultNum = sqlSessionTemplate.delete("mapperNamespace.deleteMember",memberId);
+		int resultNum = getSqlSession().delete("mapperNamespace.deleteMember",memberId);
 		return resultNum;
 	}
 	
